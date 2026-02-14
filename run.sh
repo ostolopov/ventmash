60c4c36a6221dac0ed7c2974420ae24fcf97efbd
ostolopov <egor21231bar@gmail.com>
2026-02-14 10:41:50 +0300

    initial commit

---
+++ run.sh
@@ -0,0 +1,17 @@
+set -e
+cd "$(dirname "$0")"
+VENV_DIR=".venv"
+if [ ! -d "$VENV_DIR" ]; then
+  echo "▶ Создаю виртуальное окружение ($VENV_DIR)..."
+  python3 -m venv "$VENV_DIR"
+fi
+
+echo "▶ Активация виртуального окружения..."
+source "$VENV_DIR/bin/activate"
+
+echo "▶ Установка Python-зависимостей (pip install -r requirements.txt)..."
+pip install -r requirements.txt
+
+echo "▶ Запуск Python-бэкенда..."
+python app.py
+
